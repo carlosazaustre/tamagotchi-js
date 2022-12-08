5b77304bbad5b5cf1603e8b4617a01905b8de61b
Carlos Azaustre <cazaustre@gmail.com>
2022-12-08 21:15:47 +0100

    feat: :sparkles: add hunger, happiness and health states

--- src/Tamagotchi.js
+++ src/Tamagotchi.js
@@ -1,25 +1,88 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import "./Tamagotchi.css";
+
+const THREE_SECONDS = 3000;
 
 export function Tamagotchi() {
+  const [hunger, setHunger] = useState(50);
   const [happiness, setHappiness] = useState(50);
-  const [health, setHealth] = useState(50);
+  const [health, setHealth] = useState(100);
+  const timer = useRef();
 
-  function feed() {
-    setHappiness(happiness + 10);
+  useEffect(() => {
+    timer.current = setInterval(() => {
+      setHappiness((prevHappiness) => prevHappiness - 1);
+      setHealth((prevHealth) => prevHealth - 1);
+      setHunger((prevHunger) => prevHunger - 1);
+    }, THREE_SECONDS);
+
+    return () => clearInterval(timer.current);
+  }, []);
+
+  const feed = () => {
+    setHunger((prevHunger) => prevHunger + 20);
+    setHealth((prevHealth) => prevHealth + 5);
+    setHappiness((prevHappiness) => prevHappiness + 5);
+  };
+
+  const play = () => {
+    setHappiness((prevHappiness) => prevHappiness + 20);
+    setHunger((prevHunger) => prevHunger - 5);
+    setHealth((prevHealth) => prevHealth - 5);
+  };
+
+  const sleep = () => {
+    setHunger(hunger - 5);
+    setHappiness(happiness - 5);
     setHealth(health + 10);
-  }
+  };
 
-  function play() {
-    setHappiness(happiness + 20);
-  }
+  const getProgressColor = (value) => {
+    if (value > 60) return "#00C853";
+    if (value > 20) return "#FFB300";
+    return "#B71C1C";
+  };
 
   return (
-    <div>
+    <div className="Tamagotchi">
       <h1>Tamagotchi</h1>
-      <p>Felicidad: {happiness}</p>
-      <p>Salud: {health}</p>
-      <button onClick={feed}>Alimentar</button>
-      <button onClick={play}>Jugar</button>
+      <div className="stats">
+        <div className="stat">
+          <label>Hunger:</label>
+          <div
+            className="value"
+            style={{
+              width: hunger + "%",
+              backgroundColor: getProgressColor(hunger),
+            }}
+          />
+        </div>
+        <div className="stat">
+          <label>Happiness:</label>
+          <div
+            className="value"
+            style={{
+              width: happiness + "%",
+              backgroundColor: getProgressColor(happiness),
+            }}
+          />
+        </div>
+        <div className="stat">
+          <label>Health:</label>
+          <div
+            className="value"
+            style={{
+              width: health + "%",
+              backgroundColor: getProgressColor(health),
+            }}
+          />
+        </div>
+      </div>
+      <div className="actions">
+        <button onClick={feed}>Feed</button>
+        <button onClick={play}>Play</button>
+        <button onClick={sleep}>Sleep</button>
+      </div>
     </div>
   );
 }
